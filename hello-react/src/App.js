cb413520df83e087c0a8cc39af15696b2207e919
dev-dong-su <fkdlrj1230@gmail.com>
2022-08-02 17:57:09 +0900

    8.2 7-8장

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
-import React, { createRef, forwardRef, useRef } from "react";
-import IterationSample from "./ch.6/IterationSample";
+import React, { createRef, forwardRef, useRef, useState } from "react";
+import LifeCycleSample from "./ch.7/LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 function App() {
+  const [state, setState] = useState({
+    color: "#000000",
+  });
+
+  const handleClick = () => {
+    setState({
+      color: getRandomColor(),
+    });
+  };
+
   return (
     <div>
-      <IterationSample></IterationSample>
+      <button onClick={handleClick}>랜덤 색상</button>
+      <LifeCycleSample color={state.color}></LifeCycleSample>
     </div>
   );
 }
