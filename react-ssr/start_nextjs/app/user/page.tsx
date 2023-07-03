6bec80981fa1dd52ada6cfbe618aadf32e12b330
dev-dong-su <fkdlrj1230@gmail.com>
2023-07-03 17:04:15 +0900

    next js 학습

---
+++ react-ssr/start_nextjs/app/user/page.tsx
@@ -0,0 +1,17 @@
+
+import {FC} from 'react';
+
+interface Props {
+    children?: never;
+    
+};
+
+const : FC<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ;
